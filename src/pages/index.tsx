497df748256fc6d316e78ec42b0e26767ef5f822
陈梓聪 <20804846@dw.gdbbk.com>
2022-07-11 15:29:58 +0800

    初始化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,29 @@
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import '@ckeditor/ckeditor5-build-classic/build/translations/zh-cn'
+import { CKEditor } from '@ckeditor/ckeditor5-react'
+import { useRef } from 'react'
+
 export default function HomePage() {
-  return <div>123</div>
+  const ck = useRef<any>()
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          console.log(ck.current.getData())
+        }}
+      >
+        getData
+      </button>
+
+      <CKEditor
+        config={{ language: 'zh-cn' }}
+        data="<p>Hello from CKEditor 5!</p>"
+        editor={ClassicEditor}
+        onReady={(editor) => {
+          ck.current = editor
+        }}
+      />
+    </div>
+  )
 }
